85462935e0744f26bde41eb6d8147fa21785e6e5
Ashad Jamal <ashadjamalhyt@gmail.com>
2024-05-08 01:17:39 +0530

    event section add

--- src/Components/Event/Event.jsx
+++ src/Components/Event/Event.jsx
@@ -1,8 +1,114 @@
 import React from "react";
-export default function Event(){
-    return(
-        <>
-            <div>Event</div>
-        </>
-    )
+import "../../assets/Styles/event.css";
+import pic from "../../assets/images/men1.png";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+export default function Event() {
+  return (
+    <>
+      <div className="event container-fluid">
+        <div className="row justify-content-center align-items-center text-center pt-5 pb-5">
+          <div className="col-11">
+            <h1>
+              Upcoming <span className="eventSpan">Events</span>
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+              neque minus natus deserunt quasi quas repudiandae laudantium et
+              consequatur animi!
+            </p>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-md-5 d-none d-lg-block">
+            <img src={pic} alt="" className="img-fluid" />
+          </div>
+          <div className="col-xs-12 col-md-7 pt-5 pb-5">
+            <div className="row justify-content-center">
+              <div
+                className="hfour d-flex justify-content-between"
+                style={{ maxWidth: "540px" }}
+              >
+                <div>
+                  {" "}
+                  <h4 className="date">
+                    <i className="bi bi-caret-right-fill"></i>
+                    July 20, 2024
+                  </h4>
+                </div>
+                <div>
+                  <span className="time">02:30 AM</span>
+                </div>
+              </div>
+              <div
+                className="card mb-3 mb-5 col-10"
+                style={{ maxWidth: "540px" }}
+              >
+                <div className="row g-0">
+                  <div className="col-md-4">
+                    <img
+                      src={pic}
+                      className="img-fluid rounded-start"
+                      alt="..."
+                    />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <h5 className="card-title">
+                        Tuesday Networking & lecture
+                      </h5>
+                      <p className="card-text">
+                        This is a wider card with supporting text below as a
+                        natural lead-in to additional content. This content is a
+                        little bit longer.
+                      </p>
+                      <p className="card-text">
+                        <small className="text-body-secondary">
+                          Last updated 3 mins ago
+                        </small>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                className="card mb-3 mb-5 col-10"
+                style={{ maxWidth: "540px" }}
+              >
+                <div className="row g-0">
+                  <div className="col-md-4">
+                    <img
+                      src={pic}
+                      className="img-fluid rounded-start"
+                      alt="..."
+                    />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <h5 className="card-title">
+                        Tuesday Networking & lecture
+                      </h5>
+                      <p className="card-text">
+                        This is a wider card with supporting text below as a
+                        natural lead-in to additional content. This content is a
+                        little bit longer.
+                      </p>
+                      <p className="card-text">
+                        <small className="text-body-secondary">
+                          Last updated 3 mins ago
+                        </small>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="viewmore p-5">
+                <Link className="float-end view">View More →</Link>
+              </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -1,8 +1,22 @@
 import React from "react";
-export default function Team(){
-    return(
-        <>
-            <div>My Team</div>
-        </>
-    )
+import "../../assets/Styles/team.css"
+export default function Team() {
+  return (
+    <>
+      <div className="team container-fluid">
+        <div className="row justify-content-center align-items-center text-center pt-5 pb-5">
+          <div className="col-10">
+            <h1>
+              Meet Our <span className="staff">Staff</span>
+            </h1>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam
+              sequi unde culpa tempora. Quaerat ab id cupiditate facere
+              doloremque blanditiis!
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
